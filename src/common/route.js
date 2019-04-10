96dafdccf58f1b502e664f51d99827f0172bf232
timwu <timwu@timwudeMacBook-Pro.local>
2019-04-10 14:36:46 +0800

    fix web router

--- src/common/route.js
+++ src/common/route.js
@@ -1,3 +1,4 @@
 import { Platform } from 'react-native'
 export const { Route, Link } = require('react-router-native')
-export const Router = Platform.OS === 'web' ? require('react-router-native').BrowserRouter : require('react-router-native').NativeRouter
+export const Router = Platform.OS === 'web' ? require('react-router-native').HashRouter : require('react-router-native').NativeRouter
+// BrowserRouter for nginx configured
